695b821d3aacbacd5b9cbcdbad0e3e1be4dd1219
Gabriel Bera <g.bera2710@gmail.com>
2026-09-04 17:04:58 -0300

    "fix: aplica no banco a nulabilidade de Complement em Address e Order
    - Adiciona as migrations MakeComplementNullable e
      MakeOrderComplementNullable, alterando as colunas Complement em
      Addresses e Orders para aceitar NULL
    - As entidades e o Fluent API ja refletiam Complement como opcional,
      mas a mudanca nunca havia sido aplicada ao banco via migration,
      causando falha ao tentar cadastrar um endereco sem complemento"

--- Ecommerce.Infrastructure/Migrations/20260904195701_MakeComplementNullable.Designer.cs
+++ Ecommerce.Infrastructure/Migrations/20260904195701_MakeComplementNullable.Designer.cs
@@ -4,6 +4,7 @@ using Ecommerce.Infrastructure.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Ecommerce.Infrastructure.Migrations
 {
     [DbContext(typeof(EcommerceDbContext))]
-    partial class EcommerceDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260904195701_MakeComplementNullable")]
+    partial class MakeComplementNullable
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
@@ -184,6 +187,7 @@ namespace Ecommerce.Infrastructure.Migrations
                         .HasColumnType("nvarchar(255)");
 
                     b.Property<string>("Complement")
+                        .IsRequired()
                         .HasMaxLength(255)
                         .HasColumnType("nvarchar(255)");
 
